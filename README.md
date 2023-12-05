c0cace7c6e49e09b9aa10847554a853e6849f90e
Wafaa <Wafaabahgat>
2023-12-06 02:26:44 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# js_e-commerce
+little complex responsive e-commerce web application that using localstorage and done using html5, css3 and js.
+
+# features
+<ul>
+  <li>auhtentication.</li>
+  <li>display products</li>
+  <li>showing product details</li>
+  <li>add to cart</li>
+  <li>add to favorite</li>
+  <li>profile setting page</li>
+  <li>auth user can add products put it shows localy</li>
+  <li>auth user can edit their products too</li>
+</ul>
---
+++ js/data.js
@@ -0,0 +1,136 @@
+// define the products
+let productsMom = [
+    {
+        id: 1,
+        title: "watch",
+        desc: " Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+        type: "watch",
+        imgUrl: "img/img6.jpg",
+        price: "200",
+        qty: 1,
+        me: "n",
+        love: "black"
+    },
+    {
+        id: 2,
+        title: "bed",
+        desc: " Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+        type: "furneture",
+        imgUrl: "img/img3.jpg",
+        price: "300",
+        qty: 1,
+        me: "n",
+        love: "black"
+    },
+    {
+        id: 3,
+        title: "small bed",
+        desc: " Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+        type: "other",
+        imgUrl: "img/BEBES-1.jpg",
+        price: "150",
+        qty: 1,
+        me: "n",
+        love: "black"
+    },
+    {
+        id: 4,
+        title: "chair",
+        desc: " Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+        type: "devices",
+        imgUrl: "img/chair2.jpg",
+        price: "150",
+        qty: 1,
+        me: "n",
+        love: "black"
+    },
+    {
+        id: 5,
+        title: "watch",
+        desc: " Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+        type: "watch",
+        imgUrl: "img/img6.jpg",
+        price: "200",
+        qty: 1,
+        me: "n",
+        love: "black"
+    },
+    {
+        id: 6,
+        title: "bed",
+        desc: " Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+        type: "furneture",
+        imgUrl: "img/img3.jpg",
+        price: "300",
+        qty: 1,
+        me: "n",
+        love: "black"
+    },
+    {
+        id: 7,
+        title: "small bed",
+        desc: " Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+        type: "other",
+        imgUrl: "img/BEBES-1.jpg",
+        price: "150",
+        qty: 1,
+        me: "n",
+        love: "black"
+    },
+    {
+        id: 8,
+        title: "chair",
+        desc: " Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+        type: "devices",
+        imgUrl: "img/chair2.jpg",
+        price: "150",
+        qty: 1,
+        me: "n",
+        love: "black"
+    },
+    {
+        id: 9,
+        title: "bed",
+        desc: " Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+        type: "furneture",
+        imgUrl: "img/img3.jpg",
+        price: "300",
+        qty: 1,
+        me: "n",
+        love: "black"
+    },
+    {
+        id: 10,
+        title: "small bed",
+        desc: " Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+        type: "other",
+        imgUrl: "img/BEBES-1.jpg",
+        price: "150",
+        qty: 1,
+        me: "n",
+        love: "black"
+    },
+    {
+        id: 11,
+        title: "chair",
+        desc: " Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+        type: "devices",
+        imgUrl: "img/chair2.jpg",
+        price: "150",
+        qty: 1,
+        me: "n",
+        love: "black"
+    },
+    
+    {
+        id: 12,
+        title: "watch",
+        desc: " Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+        type: "watch",
+        imgUrl: "img/img6.jpg",
+        price: "200",
+        qty: 1,
+        me: "n",
+        love: "black"
+    },
+]
